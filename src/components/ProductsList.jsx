d32548fa628e1160aa3b1ec6188fe15fe148f33e
Jose Luis Astorga Morillo <joseluastorga97@gmail.com>
2025-12-18 17:05:54 +0100

    feat(products): Change the number of items displayed per row on mobile.

--- src/components/ProductsList.jsx
+++ src/components/ProductsList.jsx
@@ -1,13 +1,17 @@
 // ProductsList.jsx
-import ProductCard from "./ProductCard"; // Importa el nuevo componente
+import ProductCard from "./ProductCard";
 
 const ProductsList = ({ products }) => {
   return (
     <div className="container my-5">
-      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+      {/* row-cols-2: 2 elementos en móvil (por defecto)
+          row-cols-md-2: 2 elementos en tablets
+          row-cols-lg-4: 4 elementos en ordenadores
+      */}
+      <div className="row row-cols-2 row-cols-md-2 row-cols-lg-4 g-4">
         {products.map((product) => (
           <div className="col" key={product.id}>
-            <ProductCard product={product} /> {/* Usa el nuevo componente */}
+            <ProductCard product={product} />
           </div>
         ))}
       </div>
