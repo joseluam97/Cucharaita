8cdc4d6f55df996a051dc46769af32102be7ae89
Jose Luis Astorga Morillo <joseluastorga97@gmail.com>
2026-08-03 18:10:43 +0200

    feat(app): Keeping the website in production under maintenance

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ const App = () => {
     }
   }, [cart, getTotalProducts, toggleBalanceo, toggleOffcanvas]);
 
-  if ((stateShop != undefined && stateShop.is_maintenance) || MODE === "production") {
+  if ((stateShop != undefined && stateShop.is_maintenance) || MODE_WEB === "production") {
     return (
       <div className="min-h-screen w-full bg-brand-cream/30 flex flex-col items-center justify-center p-6 relative overflow-hidden">
 
